1d3fd2ca5931cfe91b1bec947b10693657aaafac
Huyen Chau Nguyen <hello@chau-nguyen.de>
2016-10-18 16:30:47 -0700

    make functions inline to fix linker error

--- include/storage/io.hpp
+++ include/storage/io.hpp
@@ -61,7 +61,7 @@ void readHSGR(boost::filesystem::ifstream &input_stream,
 }
 
 // Returns the size of the timestamp in a file
-std::uint32_t readTimestampSize(boost::filesystem::ifstream &timestamp_input_stream)
+inline std::uint32_t readTimestampSize(boost::filesystem::ifstream &timestamp_input_stream)
 {
     timestamp_input_stream.seekg (0, timestamp_input_stream.end);
     auto length = timestamp_input_stream.tellg();
@@ -70,7 +70,7 @@ std::uint32_t readTimestampSize(boost::filesystem::ifstream &timestamp_input_str
 }
 
 // Reads the timestamp in a file
-void readTimestamp(boost::filesystem::ifstream &timestamp_input_stream, char *timestamp, std::size_t timestamp_length)
+inline void readTimestamp(boost::filesystem::ifstream &timestamp_input_stream, char *timestamp, std::size_t timestamp_length)
 {
     timestamp_input_stream.read(timestamp, timestamp_length * sizeof(char));
 }
